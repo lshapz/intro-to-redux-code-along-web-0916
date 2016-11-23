bb9df4c38d5b39a6d7e7ca0eda10aca4927f3211
lshapz <laurashapiro01@gmail.com>
2016-11-23 10:25:26 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {connect} from 'react-redux';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -9,10 +10,21 @@ class App extends Component {
         <div className="App-header">
           <h2>Welcome to React Shopping List</h2>
         </div>
-        <ShoppingList items={this.props.shoppingListItems} />
+        <ul className="list-group col-lg-6 col-lg-offset-3">
+          {this.props.shoppingListItems.map((item, i) => {
+            return <li key={i} className="list-group-item">{item.description}</li>
+          })}
+        </ul>
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {shoppingListItems: state.shoppingListItems}
+}
+ 
+const connector = connect(mapStateToProps)
+const connectedComponent = connector(App)
+ 
+export default connectedComponent;
